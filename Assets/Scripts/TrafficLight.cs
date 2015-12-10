9f193c3c13de13400d084dd2be09af959af456d4
James Nolan <th3daw@gmail.com>
2015-12-10 00:46:27 +0000

    Box Collisions
    Tying updates and variable changes to collision detecion

---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrafficLight : MonoBehaviour
+{
+
+    public bool green = false;
+    double destroy = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (green)
+            destroy -= Time.deltaTime;
+
+        if (destroy < 0)
+            Destroy(this.gameObject);
+    }
+
+    void onMouseDown()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+            green = true;
+    }
+}
